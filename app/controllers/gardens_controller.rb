e6037682b01cb32fc4bbaa8540dbdef5b54d0719
Hashim Gari <hashmaster3k@gmail.com>
2020-10-29 16:03:23 -0600

    Add new garden view page and form

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,11 +1,22 @@
 class GardensController < ApplicationController
   before_action :require_user
-  
+
   def new; end
+
+  def create
+    # hit the POST api/v1/gardens API to create a garden using strong params
+    redirect_to dashboard_path
+  end
 
   def edit; end
 
   def destroy
     redirect_to dashboard_path
+  end
+
+  private
+
+  def garden_params
+    params.permit(:name, :latitude, :longitude, :private)
   end
 end
