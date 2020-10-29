45a7dcdb6d9a21291ae5e8ecb0bf09e19f9e0598
Hashim Gari <hashmaster3k@gmail.com>
2020-10-29 12:05:29 -0600

    Move require_user into application controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,6 @@
 class DashboardController < ApplicationController
   before_action :require_user
 
-  def require_user
-    render file: "/public/404" unless current_user
-  end
-
   def show
 
   end
