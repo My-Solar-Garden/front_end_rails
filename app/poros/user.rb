a40df963226c00c701ab735c2416f86df7f05ebc
Hashim Gari <hashmaster3k@gmail.com>
2020-10-29 12:04:25 -0600

    Sad-path for visitor

---
+++ app/poros/user.rb
@@ -0,0 +1,8 @@
+class User
+  attr_reader :id, :email
+
+  def initialize(data)
+    @id = data[:id]
+    @email = data[:attributes][:email]
+  end
+end
--- spec/features/dashboard/show_spec.rb
+++ spec/features/dashboard/show_spec.rb
@@ -1,9 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'User Dashboard' do
-  describe 'As a logged in user' do
-    it 'I can visit my dashboard' do
+  describe 'a logged in user' do
+    before :each do
+      user = User.new({id: 1, attributes: {email: '123@gmail.com'}})
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    end
+
+    it 'can visit their dashboard' do
       visit '/dashboard'
+    end
+  end
+
+  describe 'a visitor' do
+    it 'cannot visit a dashboard' do
+      visit '/dashboard'
+      expect(page).to have_content("The page you were looking for doesn't exist.")
     end
   end
 end
