875dbc242fc08105d884ccc81f7169a37e88a89f
Hashim Gari <hashmaster3k@gmail.com>
2020-10-29 13:47:20 -0600

    Add image and buttons for gardens

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -2,4 +2,6 @@ class GardensController < ApplicationController
   before_action :require_user
 
   def index; end
+
+  def new; end
 end
--- spec/features/dashboard/show_spec.rb
+++ spec/features/dashboard/show_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe 'User Dashboard' do
   describe 'a logged in user' do
     before :each do
-      user = User.new({id: 1,
+      @user = User.new({id: 1,
                       attributes: { email: '123@gmail.com' },
                       relationships: { gardens: { data: [] }}})
 
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
     end
 
     it 'can visit their dashboard' do
@@ -16,9 +16,29 @@ RSpec.describe 'User Dashboard' do
 
     it 'sees a CTA to create a new garden if they have none' do
       visit '/gardens'
-
       expect(page).to have_content('You have no gardens')
       expect(page).to have_button('Add New Garden')
+    end
+
+    it 'has a button to create a new garden' do
+      visit '/gardens'
+      click_button('Add New Garden')
+      expect(current_path).to eq(new_garden_path)
+    end
+
+    it 'displays the users gardens' do
+      @user.gardens << {id: 1, type: 'garden'}
+      @user.gardens << {id: 2, type: 'garden'}
+      visit '/gardens'
+      expect(page).to have_css('.garden', count: 2)
+
+      within '#garden-1' do
+        expect(page).to have_css('.garden-button', count: 2)
+      end
+
+      within '#garden-2' do
+        expect(page).to have_css('.garden-button', count: 2)
+      end
     end
   end
 
