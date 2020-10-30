4b6623bd8ab601ce72f17ae68d432f4bd6889051
Norma Lopez <norma_l304@yahoo.com>
2020-10-30 00:14:59 -0600

    Fix syntax error; move comma outside of quotes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "home#index"
-  
+
   get '/auth/:provider/callback', to: 'sessions#create'
-  get 'auth/failure,' to: redirect('/')
+  get 'auth/failure', to: redirect('/')
 
   get '/privacy', to: 'privacy#index'
 end
