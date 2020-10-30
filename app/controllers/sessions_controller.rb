22fdc3fa6ca4e23764b8ec5afda136ae90ca7d31
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-30 17:34:40 -0600

    Update sessions controller with destroy method
    Co-authored-by: Dani Coleman<danicoleman00@gmail.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,10 @@
 class SessionsController < ApplicationController
 
-  def create
+  def create; end
 
-
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You've made an impact today.  We hope to see you again tomorrow."
+    redirect_to root_path
   end
-
-
 end
