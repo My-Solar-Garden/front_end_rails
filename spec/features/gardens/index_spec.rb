7bf267223454004e289e442c6fcbb8d740392dd4
Daniel Lessenden <dless27@gmail.com>
2020-10-31 16:23:52 -0500

    Create a test spec outline.

---
+++ spec/features/gardens/index_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'New Garden Page' do
+  describe 'a logged in user' do
+    before :each do
+      @user = User.new({id: 1,
+                      attributes: {
+                          email: '123@gmail.com' },
+                      relationships: {
+                          gardens: {
+                              data: [ { id: 1,
+                                        attributes: {
+                                            name: 'My Garden',
+                                            latitude: 23.0,
+                                            longitude: 24.0,
+                                            description: 'Simple Garden',
+                                            private: false }} ] }}})
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+    end
+  end
+end 
