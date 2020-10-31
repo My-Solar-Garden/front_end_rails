0f53a04f98720c658436ba21ea5f8398344b77c0
Daniel Lessenden <dless27@gmail.com>
2020-10-31 16:11:14 -0500

    Add test for public gardens

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -69,6 +69,7 @@ RSpec.describe 'Welcome' do
       visit root_path
 
       click_link "Be The Change. Learn More"
+
       expect(current_path).to eq(learn_more_path)
     end
 
@@ -76,6 +77,14 @@ RSpec.describe 'Welcome' do
       visit root_path
 
       expect(page).to have_link("Login with Google")
+    end
+
+    it "has a link to pbulic gardens (gardens index page)" do
+      visit root_path
+      expect(page).to have_link("Garden Search")
+      click_link "Garden Search"
+      save_and_open_page
+      expect(current_path).to eq('/gardens')
     end
     # We are not sure how to test this quite yet. I think once the OAuth is complete this can be tested.
     xit "expects to be sent to the Google auth when the button is clicked" do
