4c21fe42b4083862e1454cc9b39a21e0dd89fc60
Leah Riffell <leah.riffell@gmail.com>
2020-10-31 15:09:35 -0600

    Add user poro spec and method for finding specific user from backend

--- spec/features/sessions/new_spec.rb
+++ spec/features/sessions/new_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Sessions Controller', type: :feature do
   describe "As a visitor" do
-    VCR.use_cassette('google_oauth') do
-      it "I can sign in with Google OAuth" do
+    it "I can sign in with Google OAuth" do
+      VCR.use_cassette('google_oauth') do
         visit root_path
         stub_omniauth
         click_link 'Login with Google'
