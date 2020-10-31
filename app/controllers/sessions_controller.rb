8370e0d05f5b165fcb5c72aa57f5652518cd8ed4
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-30 18:27:49 -0600

    Update sessions controller with message about why it is how it is
    Co-authored-by: Dani Coleman<danicoleman00@gmail.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,11 @@
 class SessionsController < ApplicationController
 
-  def create; end
+  #we recognize these may not look the same once OAuth is in place, but we put them in place to attempt to test with them.  Turns out if you do the ApplicationController shortcut to make :current_user equal @user, then even if you do the destroy action, there isn't really a way to destroy the session.
+
+  def create;end
 
   def destroy
-    session[:user_id] = nil
+    #session[:user_id] = nil
     flash[:success] = "You've made an impact today.  We hope to see you again tomorrow."
     redirect_to root_path
   end
