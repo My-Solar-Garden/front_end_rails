1a5c2aecf3b94abb3d1a11774e97625d361d17d7
Norma Lopez <norma_l304@yahoo.com>
2020-10-31 01:35:45 -0600

    Update create action to create session from User object

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,12 @@
+require 'google/api_client/client_secrets.rb'
+
 class SessionsController < ApplicationController
   def create
     conn = Faraday.new(url: "#{ENV['RAILS_ENGINE_DOMAIN']}")
     response = conn.post('/api/v1/users', auth_hash)
     json = JSON.parse(response.body, symbolize_names: true)
-    user_id = json[:data][:id]
-    session[:user_id] = user_id
+    user = User.new(json[:data])
+    session[:user_id] = user.id
     flash[:notice] = 'You have successfully logged in'
     redirect_to dashboard_path
   end
