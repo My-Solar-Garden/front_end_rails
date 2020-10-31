d87bed3f23c6d888c32fc4dd805f8bb1093aeb28
Norma Lopez <norma_l304@yahoo.com>
2020-10-30 18:30:43 -0600

    Update spec; remove allow_instance_of and just use stub_omniauth instead

--- spec/features/sessions/new_spec.rb
+++ spec/features/sessions/new_spec.rb
@@ -1,18 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'Sessions Controller', type: :feature do
-    describe "As a visitor" do
-        it "I can sign in with Google OAuth" do
-            allow_any_instance_of(SessionsController).to receive(:auth_hash).and_return(stub_omniauth)
-            visit root_path
-            click_link 'Login with Google'
-            expect(page).to have_content('You have successfully logged in.')
-        end
-
-        it "I fail to sign in with Google OAuth" do
-            visit root_path
-            click_link 'Login with Google'
-            expect(page).to have_content('Google Authorization was unable to be completed.')
-        end
+  describe "As a visitor" do
+    it "I can sign in with Google OAuth" do
+      visit root_path
+      stub_omniauth
+      click_link 'Login with Google'
+      expect(page).to have_content('You have successfully logged in')
     end
-end 
+  end
+end
