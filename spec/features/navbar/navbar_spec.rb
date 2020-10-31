33a698b1036562c93e6d08dfeceecf2acd337844
Daniel Lessenden <dless27@gmail.com>
2020-10-31 16:18:50 -0500

    Delete a save and open page

--- spec/features/navbar/navbar_spec.rb
+++ spec/features/navbar/navbar_spec.rb
@@ -187,7 +187,7 @@ RSpec.describe 'Navbar' do
 
   it "can see my gardens, my impact, learn more, profile and logout on learn more page" do
     visit "/learn_more"
-    save_and_open_page
+  
     within "#navbar-#{@user.id}" do
       expect(page).to have_link('My Gardens')
       expect(page).to have_link('My Impact')
