026da521f3f3d501c3764170bebef8d012246e2c
Norma Lopez <norma_l304@yahoo.com>
2020-10-31 01:35:01 -0600

    Add omniauth middleware

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
+  {
+    access_type: 'offline',
+    prompt: 'consent',
+    select_account: true
+  }
 end
