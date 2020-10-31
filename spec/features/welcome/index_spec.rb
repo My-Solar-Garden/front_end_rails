a54c2175976a7d2a29c99588475956f2c19e892b
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-30 18:26:08 -0600

    Update welcome index spec to create user and garden before
    Co-authored-by: Dani Coleman<danicoleman00@gmail.com>

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -2,6 +2,29 @@ require 'rails_helper'
 
 RSpec.describe 'Welcome' do
   describe 'a visitor' do
+    before :each do
+      @garden = { id: 1,
+                attributes: {
+                    name: 'My Garden',
+                    latitude: 23.0,
+                    longitude: 24.0,
+                    description: 'Simple Garden',
+                    private: false },
+                relationships: { plants: {
+                                    data: []},
+                                 sensors: {
+                                    data: []}}}
+
+      @user = User.new({id: 1,
+                      attributes: {
+                          email: '123@gmail.com' },
+                      relationships: {
+                          gardens: {
+                              data: [ @garden ] }}})
+
+      @garden = @user.gardens.first
+    end
+
 
 
   end
