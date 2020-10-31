b105bb160fef076c2dd6ab10cde6a143fd86da1c
Norma Lopez <norma_l304@yahoo.com>
2020-10-31 10:24:49 -0600

    Change home controller to welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "home#index"
+  root "welcome#index"
 
   get '/auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
