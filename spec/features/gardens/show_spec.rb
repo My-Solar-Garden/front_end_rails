7dad1c90218a773943a0deb29763ce0467f0007d
Angela Guardia <angelaguardia20@gmail.com>
2020-10-31 13:00:16 -0700

    Add sensors test to garden show page spec

--- spec/features/gardens/show_spec.rb
+++ spec/features/gardens/show_spec.rb
@@ -1,26 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe 'Show Garden Page' do
-  describe 'a logged in user' do
+  describe 'a logged in user with no plants or sensors' do
     before :each do
       @garden = { id: 1,
-                attributes: {
+                  attributes: {
                     name: 'My Garden',
                     latitude: 23.0,
                     longitude: 24.0,
                     description: 'Simple Garden',
-                    private: false },
-                relationships: { plants: {
-                                    data: []},
-                                 sensors: {
-                                    data: []}}}
+                    private: false
+                  },
+                  relationships: {
+                      plants: {data: []},
+                      sensors: {data: []}
+                  }
+                }
 
-      @user = User.new({id: 1,
-                      attributes: {
-                          email: '123@gmail.com' },
-                      relationships: {
-                          gardens: {
-                              data: [ @garden ] }}})
+      @user = User.new({  id: 1,
+                          attributes: { email: '123@gmail.com' },
+                          relationships: {
+                            gardens: {data: [@garden]}
+                            }
+                        })
 
       @garden = @user.gardens.first
 
@@ -42,6 +44,70 @@ RSpec.describe 'Show Garden Page' do
       within '.garden-sensors' do
         expect(page).to have_content('You have no sensors')
         expect(page).to have_button('Add Sensor')
+      end
+    end
+  end
+
+  describe 'a logged in user with plans and sensors' do
+    before :each do
+      @sensor1 = {:id=> 1,
+                  :type=>"sensor",
+                  :attributes=>{
+                    :min_threshold=>2,
+                    :max_threshold=>15,
+                    :sensor_type=>"moisture"
+                    },
+                  :relationships=>{
+                    :garden=>{:data=>{:id=> 1, :type=>"garden"}}, :garden_healths=>{:data=>[]}
+                    }
+                  }
+
+    @sensor2 = {:id=> 2,
+                :type=>"sensor",
+                :attributes=>{
+                  :min_threshold=>2,
+                  :max_threshold=>15,
+                  :sensor_type=>"moisture"
+                  },
+                :relationships=>{
+                  :garden=>{:data=>{:id=> 1, :type=>"garden"}}, :garden_healths=>{:data=>[]}
+                  }
+                }
+
+      @garden = { id: 1,
+                  attributes: {
+                    name: 'My Garden',
+                    latitude: 23.0,
+                    longitude: 24.0,
+                    description: 'Simple Garden',
+                    private: false
+                    },
+                  relationships: {
+                    plants: {data: []},
+                    sensors: {data: [@sensor1, @sensor2]}
+                    }
+                }
+
+      @user = User.new({id: 1,
+                        attributes: { email: '123@gmail.com' },
+                        relationships: {
+                          gardens: {data: [@garden]}
+                          }
+                        })
+
+      @garden = @user.gardens.first
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+    end
+
+    it "displays all sensors realated to a garden" do
+      visit "/gardens/#{@garden[:id]}"
+
+      within '.garden-sensors' do
+        expect(page).to have_content(@sensor1[:id])
+        expect(page).to have_content(@sensor1[:attributes][:sensor_type])
+        expect(page).to have_content(@sensor2[:id])
+        expect(page).to have_content(@sensor2[:attributes][:sensor_type])
       end
     end
   end
