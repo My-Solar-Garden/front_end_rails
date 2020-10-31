4009f0fe50f26db05541c2a092e68019fa930427
Daniel Lessenden <dless27@gmail.com>
2020-10-31 16:10:15 -0500

    Add garden index route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get '/privacy', to: 'privacy#index'
   resources :users, except: [:index]
   get "/dashboard", to: "dashboard#show"
-  resources :gardens, except: [:index]
+  resources :gardens
   resources :plants, except: [:index]
   resources :sensors, except: [:index]
   get "/learn_more", to: "learn_more#show"
