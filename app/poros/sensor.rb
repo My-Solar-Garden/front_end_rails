7853c6fcbcc04559de402c67effe3520abdc20f3
Angela Guardia <angelaguardia20@gmail.com>
2020-10-31 17:39:34 -0700

    Correct access to nested data structure to get sensor's garden id

--- app/poros/sensor.rb
+++ app/poros/sensor.rb
@@ -4,10 +4,10 @@ class Sensor
               :sensor_type,
               :min_threshold,
               :max_threshold
-              
+
   def initialize(data)
     @id = data[:id]
-    @garden_id = data[:attributes][:garden_id]
+    @garden_id = data[:relationships][:garden][:data][:id]
     @sensor_type = data[:attributes][:sensor_type]
     @min_threshold = data[:attributes][:min_threshold]
     @max_threshold = data[:attributes][:max_threshold]
