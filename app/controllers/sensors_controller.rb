92bf60b9a0d07887759ecd426713cae1227b0c07
Daniel Lessenden <dless27@gmail.com>
2020-11-01 01:20:31 -0500

    Create a redirect to dashboard after creation

--- app/controllers/sensors_controller.rb
+++ app/controllers/sensors_controller.rb
@@ -1,7 +1,11 @@
 class SensorsController < ApplicationController
   before_action :require_user
   def show; end
-  def new; end
+  def new
+  end
+  def create
+    redirect_to dashboard_path
+  end
   def edit; end
   def destroy
     redirect_to dashboard_path
