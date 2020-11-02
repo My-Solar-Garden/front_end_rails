e3e6365e00038e9cff5787b6fb6e0207a31c1005
Daniel Lessenden <dless27@gmail.com>
2020-11-02 14:18:53 -0600

    Put hardocded data in the conn.post block. This will create a sensor but is not dynamic

--- app/controllers/sensors_controller.rb
+++ app/controllers/sensors_controller.rb
@@ -12,9 +12,9 @@ class SensorsController < ApplicationController
     conn.post do |req|
       req.url 'https://solar-garden-be.herokuapp.com/api/v1/sensors'
       req.headers['Content-Type'] = 'application/json'
+      req.body = '{"sensor_type":0, "min_threshold":5, "max_threshold":10, "garden_id":1}'
     end
 
-    # req.body = '{"sensor_type":0, "min_threshold":5, "max_threshold":10, "garden_id":1}'
     # req.body = sensor_params.to_s
     # req.body = "{'sensor_type': #{params[:sensor_type]}, 'min_threshold': #{params[:min_threshold]}, 'max_threshold': #{params[:max_threshold]}, 'garden_id': #{params[:garden_id]}}"
 
