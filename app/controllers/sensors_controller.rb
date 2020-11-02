9cd2b4691ad047eb215d35092016d7cafc1232bd
Daniel Lessenden <dless27@gmail.com>
2020-11-02 14:17:59 -0600

    Can receive a status 202 for sensor create, however the data is hard coded. Having trouble making the action dynamic

--- app/controllers/sensors_controller.rb
+++ app/controllers/sensors_controller.rb
@@ -7,24 +7,17 @@ class SensorsController < ApplicationController
   end
 
   def create
-    binding.pry
-
     conn = Faraday.new(:url => "https://solar-garden-be.herokuapp.com/api/v1/sensors")
-
+    binding.pry
     conn.post do |req|
       req.url 'https://solar-garden-be.herokuapp.com/api/v1/sensors'
       req.headers['Content-Type'] = 'application/json'
-      req.body = '{"sensor_type":0, "min_threshold":5, "max_threshold":10, "garden_id":1}'
     end
 
-    sensor_params
-    # Farady.new(url: 'https://solar-garden-be.herokuapp.com/api/v1/sensors').post('')
-    # conn = Faraday.new("https://solar-garden-be.herokuapp.com/api/v1/sensors")
-    # binding.pry
-    # response = conn.post do |req|
-    #   req.params = sensor_params
-    #   req.headers['Content-Type'] = 'application/json'
-    # end
+    # req.body = '{"sensor_type":0, "min_threshold":5, "max_threshold":10, "garden_id":1}'
+    # req.body = sensor_params.to_s
+    # req.body = "{'sensor_type': #{params[:sensor_type]}, 'min_threshold': #{params[:min_threshold]}, 'max_threshold': #{params[:max_threshold]}, 'garden_id': #{params[:garden_id]}}"
+
     redirect_to "/gardens/#{@garden_id}"
   end
 
