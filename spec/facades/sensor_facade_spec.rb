0c8413518760cf059f6ad119af589227a1d833b4
Angela Guardia <angelaguardia20@gmail.com>
2020-11-03 14:53:57 -0800

    Stub out API calls for show_garden_spec tests

--- spec/facades/sensor_facade_spec.rb
+++ spec/facades/sensor_facade_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 describe SensorFacade do
-  it "should return sensor details for specific sensor" do
-    sensor_params = {sensor_type: 0, min_threshold: 5, max_threshold: 10, garden_id: 1}
+  it "can generate a new sensor" do
+    sensor_params = {sensor_type: 'moisture', min_threshold: 5, max_threshold: 10, garden_id: 1}
+
+    new_sensor = File.read('spec/fixtures/new_sensor.json')
+
+    stub_request(:post, "https://solar-garden-be.herokuapp.com/api/v1/sensors?garden_id=#{sensor_params[:garden_id]}&sensor_type=#{sensor_params[:sensor_type]}&min_threshold=#{sensor_params[:min_threshold]}&max_threshold=#{sensor_params[:max_threshold]}").to_return(status: 200, body: new_sensor, headers: {})
+
     sensor = SensorFacade.new_sensor(sensor_params)
-    excpet(SensorFacde.all.count).to eq(1)
+
+    expect(sensor).to be_a(Sensor)
   end
 end
