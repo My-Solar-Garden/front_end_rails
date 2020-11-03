6642da63bd14154a13e15a1866df74ead775c3d2
Dani Coleman <danicoleman00@gmail.com>
2020-11-03 16:05:13 -0700

    no changes

---
+++ spec/facades/sensor_facade_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe SensorFacade do
+  it "should return sensor details for specific sensor" do
+    params = {id: 1}
+    garden = SensorFacade.sensor_details(params)
+
+    expect(sensor).to be_a(Sensor)
+  end
+
+  it "should generate sensor poros" do
+    params = {:data=>
+                    {:id=>"1",
+                     :type=>"sensor",
+                     :attributes=>{
+                       :min_threshold=>1,
+                       :max_threshold=>4,
+                       :sensor_type=>"light",
+                     :relationships=>{
+                       :garden=>{
+                         :data=>[]},
+                         :garden_healths=>{
+                           :data=>[]}}}}
+            }
+    sensor = SensorFacade.sensor_details(params)
+  end
+end
