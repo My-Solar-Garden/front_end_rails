d057a90dd61b214163f7e93acbcfc98b477cdd81
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-02 21:28:32 -0700

    Update session service to make methods work
    Co-authored-by: Dani Coleman<danicoleman00@gmail.com>

--- app/services/session_service.rb
+++ app/services/session_service.rb
@@ -1,16 +1,14 @@
 class SessionService
-  def self.session_details(params, auth_hash)
-    get_parsed_json("api/v1/users/#{params[:id]}", auth_hash)
+  def self.session_details(auth_hash)
+    get_parsed_json("/api/v1/users", auth_hash)
   end
 
-  def self.get_parsed_json(url, auth_hash, params = {})
-    response = conn.post(url, auth_hash) do |req|
-      req.params = params
-    end
+  def self.get_parsed_json(url, auth_hash)
+    response = conn.post(url, auth_hash)
     JSON.parse(response.body, symbolize_names: true)
   end
 
   def self.conn
-    Faraday.new(url: 'https://solar-garden-be.herokuapp.com/' )
+    Faraday.new(url: "#{ENV['BE_URL']}" )
   end
 end
