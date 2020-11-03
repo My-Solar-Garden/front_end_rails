fba75fd1ff24c2a794ad67ff22219997960b76d3
Dani Coleman <danicoleman00@gmail.com>
2020-11-03 13:22:21 -0700

    Add object call from garden facade in show action
    Co-authored-by: Logan Riffell<lkriffell@gmail.com>

--- app/controllers/sensors_controller.rb
+++ app/controllers/sensors_controller.rb
@@ -2,8 +2,7 @@ class SensorsController < ApplicationController
   before_action :require_user
 
   def show
-    @sensor = GardenFacade.sensor_details
-    require "pry"; binding.pry
+    @sensor = GardenFacade.sensor_details(params)
   end
 
   def new; end
