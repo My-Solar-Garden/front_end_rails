4c8e7e0e12bf68156b55cbb47b51c34a1c065e26
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-04 15:37:48 -0700

    Update routes for plant show

--- config/routes.rb
+++ config/routes.rb
@@ -24,5 +24,6 @@ Rails.application.routes.draw do
     post '/gardens/:garden_id/sensors', to: 'sensors#create'
     delete '/gardens/:garden_id/sensors/:id', to: 'sensors#destroy'
     get '/gardens/:garden_id/sensors/:id', to: 'sensors#show'
+    get '/gardens/:garden_id/plants/:id', to: 'plants#show'
   # end
 end
