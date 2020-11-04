8a5b1fd8d5fde483292d437482d1e50c0f5913ef
Dani Coleman <danicoleman00@gmail.com>
2020-11-04 16:09:09 -0700

    Change the symbols to quotes

--- spec/services/garden_service_spec.rb
+++ spec/services/garden_service_spec.rb
@@ -11,7 +11,7 @@ describe GardenService do
     params = {"name"=>"The Grove", "latitude"=>"71.0", "longitude"=>"25.0", "private"=>"false", "description"=>"My first garden"}
 
     expected_output = File.read('spec/fixtures/new_garden.json')
-    stub_request(:post, "https://solar-garden-be.herokuapp.com/api/v1/gardens?description=#{params[:description]}&latitude=#{params[:latitude]}&longitude=#{params[:longitude]}&name=#{params[:name]}&private=false&user_id=1").to_return(status: 200, body: expected_output, headers: {})
+    stub_request(:post, "#{ENV['BE_URL']}/api/v1/gardens?description=#{params['description']}&latitude=#{params['latitude']}&longitude=#{params['longitude']}&name=#{params['name']}&private=false&user_id=1").to_return(status: 200, body: expected_output, headers: {})
 
     response = GardenService.new_garden(params.symbolize_keys, "1")
 
