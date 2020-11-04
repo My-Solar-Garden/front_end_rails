a8e67009ad73e36dbc00044d363a0191cfac47cf
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-04 15:30:28 -0700

    Update sensors new_spec to test a visitor getting a 404 when visiting

--- spec/features/sensors/new_spec.rb
+++ spec/features/sensors/new_spec.rb
@@ -1,6 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe 'New Sensor Page' do
+  describe "a visitor" do
+    before :each do
+      @public_garden = Garden.new({ id: 7,
+                attributes: {
+                    name: 'Cole Community Garden',
+                    latitude: 39.45,
+                    longitude: -104.58,
+                    description: 'A diverse, dedicated group of students and neighbors who believe in bettering ourselves, our food supply and our community through urban gardening.',
+                    private: false },
+                relationships: { plants: {
+                                    data: []},
+                                  users: {
+                                      data: [{id: "1", type: "user"}]},
+                                 sensors: {
+                                    data: []}}})
+      @user = User.new({id: 1,
+                      attributes: {
+                          email: '123@gmail.com' },
+                      relationships: {
+                          gardens: {
+                              data: [ @public_garden ] }}})
+
+      @garden = @user.gardens.first
+    end
+
+    it "cannot visit page" do
+      visit "/gardens/#{@garden.id}/sensors"
+      expect(page).to have_content("The page you were looking for doesn't exist.")
+    end
+  end
+
   describe 'a logged in user' do
     before :each do
       @public_garden = Garden.new({ id: 7,
