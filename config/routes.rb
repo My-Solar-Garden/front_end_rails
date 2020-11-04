3e68c6c94cbe999d936b72e0935e443744e9c877
Angela Guardia <angelaguardia20@gmail.com>
2020-11-04 08:21:26 -0800

    Add sensor delete route

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,10 @@ Rails.application.routes.draw do
   get '/profile', to: 'users#show'
 
   resources :users, except: [:index, :show]
-  resources :gardens, except: [:index]
+  # resources :gardens, except: [:index] do
+  #   resources :sensors, only: [:destroy]
+  #   require "pry"; binding.pry
+  # end
   resources :plants, except: [:index]
   resources :sensors, except: [:index]
   get "/learn_more", to: "learn_more#show"
@@ -21,5 +24,6 @@ Rails.application.routes.draw do
   # namespace :gardens do
     get '/gardens/:garden_id/sensors', to: 'sensors#new'
     post '/gardens/:garden_id/sensors', to: 'sensors#create'
+    delete '/gardens/:garden_id/sensors/:id', to: 'sensors#destroy'
   # end
 end
