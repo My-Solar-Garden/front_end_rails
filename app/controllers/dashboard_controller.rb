7bc2558103643ee99403f3ea9c026e6c91f90105
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-04 21:39:43 -0700

    Update dash controller to send current_user.location to WeatherFacade ... for weather info

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,8 @@
 class DashboardController < ApplicationController
   before_action :require_user
-  
+
   def show
     @gardens = GardenFacade.create_garden_objects(current_user.gardens)
+    @weather = WeatherFacade.create_weather_objects(current_user.location)
   end
 end
