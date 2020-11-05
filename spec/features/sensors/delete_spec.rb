aef5618b80190b4b2f2f9dd4902734999d84cd5d
Logan Riffell <lkriffell@gmail.com>
2020-11-05 13:53:58 -0700

    Fix test

--- spec/features/sensors/delete_spec.rb
+++ spec/features/sensors/delete_spec.rb
@@ -8,20 +8,46 @@ RSpec.describe 'On the garden show page' do
         email: 'planter@gmail.com' },
         relationships: {
           gardens: {
-            data: [ {id: '3', type: 'garden'}, {id: '4', type: 'garden'}] }}})
+            data: [ {id: '1', type: 'garden'}] }}})
+
+      Sensor.new({
+                    id: "1",
+                    type: "sensor",
+                    attributes: {
+                        min_threshold: 5,
+                        max_threshold: 15,
+                        sensor_type: "moisture"
+                    },
+                    relationships: {
+                        garden: {
+                            data: {
+                                id: "1",
+                                type: "garden"
+                            }
+                        },
+                        garden_healths: {
+                            data: []
+                        }
+                    }
+                })
+
+      new_garden = File.read('spec/fixtures/new_garden.json')
+      sensor = File.read('spec/fixtures/sensor.json')
+      stub_request(:get, "#{ENV['BE_URL']}/api/v1/gardens/43").to_return(status: 200, body: new_garden, headers: {})
+      stub_request(:get, "#{ENV['BE_URL']}/api/v1/sensors/1").to_return(status: 200, body: sensor, headers: {})
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_with_gardens)
     end
 
     it "I can delete a sensor", :vcr do
-      visit garden_path(1)
+      visit garden_path(4)
 
       sensors = page.all('.sensor')
       total = sensors.size
 
       first('.sensor').click_button('Delete')
 
-      expect(current_path).to eq(garden_path(1))
+      expect(current_path).to eq(garden_path(4))
       expect(page.all('.sensor').size).to eq(total - 1)
     end
   end
