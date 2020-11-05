8b7439cbc8478fee01d537dfcdac24b1cae6d25c
Angela Guardia <angelaguardia20@gmail.com>
2020-11-04 18:04:19 -0800

    Add GardenHealthService

---
+++ spec/services/garden_health_service_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+RSpec.describe GardenHealthService do
+  xit "returns the last reading" do
+    attr = {
+    "data": {
+        "id": "89",
+        "type": "sensor",
+        "attributes": {
+            "min_threshold": 5.0,
+            "max_threshold": 14.0,
+            "sensor_type": "temperature"
+        },
+        "relationships": {
+            "garden": {
+                "data": {
+                    "id": "1",
+                    "type": "garden"
+                }
+            },
+            "garden_healths": {
+                "data": []
+            }
+        }
+    }}
+
+    sensor = Sensor.new(attr.deep_symbolize_keys[:data])
+
+    reading = GardenHealthService.last_reading(sensor)
+    require "pry"; binding.pry
+    expect(reading).to eq('99')
+  end
+end
