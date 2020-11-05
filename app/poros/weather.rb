b1e625acc0e67f89545eafcdd682e379260e5061
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-05 11:59:45 -0700

    Add weather poro created by Weather API team

---
+++ app/poros/weather.rb
@@ -0,0 +1,18 @@
+class Weather
+  attr_reader :temperature,
+              :humidity,
+              :description,
+              :daily
+  def initialize(data)
+    @temperature = data[:current][:temp]
+    @humidity = data[:current][:humidity]
+    @description = data[:current][:weather][0][:description].titleize
+    @daily = eight_day_forecast(data[:daily])
+  end
+
+  def eight_day_forecast(data)
+    data.map do |day|
+      Daily.new(day)
+    end
+  end
+end
