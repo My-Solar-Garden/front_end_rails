f69b302fa74c04282fed6d32688ef00f3bc0d9ef
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-11-18 16:29:12 -0700

    Undo changes to weather and garden show d/t front end styling wanting .. 8 days now

--- app/poros/weather.rb
+++ app/poros/weather.rb
@@ -8,10 +8,10 @@ class Weather
     @temperature = data[:data][:attributes][:temperature]
     @humidity = data[:data][:attributes][:humidity]
     @description = data[:data][:attributes][:description].titleize
-    @daily = seven_day_forecast(data[:data][:attributes][:daily])[1..-1]
+    @daily = eight_day_forecast(data[:data][:attributes][:daily])
   end
 
-  def seven_day_forecast(data)
+  def eight_day_forecast(data)
     data.map do |day|
       DailyForecast.new(day)
     end
